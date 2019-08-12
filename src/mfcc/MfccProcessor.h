9db83e226ecd895d81413260e347195c5103988f
Yinlong Su <suyinlong07@gmail.com>
2019-08-11 21:40:44 -0700

    Add MfccProcessor header

---
+++ src/mfcc/MfccProcessor.h
@@ -0,0 +1,35 @@
+#ifndef VOICE_MFCC_MFCCPROCESSOR_H
+#define VOICE_MFCC_MFCCPROCESSOR_H
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+namespace voice {
+  class MfccProcessor {
+  public:
+    explicit MfccProcessor(int frameLength);
+    ~MfccProcessor();
+  private:
+    static constexpr float PI = 4.0F * std::atan(1.0F);
+    static constexpr float TWO_PI = 8.0F * std::atan(1.0F);
+
+    const int frameLength_;
+    std::vector<double> hamming; // Hamming window
+    std::vector<float> data; // Data
+
+    void initHamming();
+    void processHamming(std::vector<short>& frame);
+    float frameEnergy();
+    void computeZeroFft();
+    void computeFft();
+    void computeMagSquare();
+    void computeMelEN();
+    void computeCepstrum();
+
+  };
+} // namespace voice
+
+#endif // VOICE_MFCC_MFCCPROCESSOR_H
