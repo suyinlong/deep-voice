711c9bd042976a00139b2897ed0fbc65debbfff1
Yinlong Su <suyinlong07@gmail.com>
2019-08-11 20:39:18 -0700

    Add MfccFilter to generate filterWeight matrix

---
+++ src/mfcc/MfccFilter.h
@@ -0,0 +1,30 @@
+#ifndef VOICE_MFCC_MFCCFILTER_H
+#define VOICE_MFCC_MFCCFILTER_H
+
+#include <cmath>
+#include <vector>
+
+namespace voice {
+  class MfccFilter {
+  public:
+    explicit MfccFilter(int samplingRate, int frameLength);
+    ~MfccFilter();
+    void init();
+    std::vector<std::vector<float>>& getFilter();
+  private:
+    static constexpr float TWO_PI = 8.0F * std::atan(1.0F);
+    static const int FILTER_NUM = 26; // Number of filters
+    static const unsigned long FFT_LEN = 512; // FFT points
+
+    const int samplingRate_;
+    const int frameLength_;
+    const int highLimit_;
+    const int lowLimit_;
+    std::vector<std::vector<float>> filterWeight; // Mel filterbank weights
+
+    void initFilter();
+    void createFilter(int rate, int high, int low);
+  };
+} // namespace voice
+
+#endif // VOICE_MFCC_MFCCFILTER_H
