910a74fa2c28decbb572f2fa508c8e97d5855cdf
Yinlong Su <suyinlong07@gmail.com>
2019-08-11 18:57:06 -0700

    Implement WavReader

--- src/mfcc/WavReader.cc
+++ src/mfcc/WavReader.cc
@@ -1,1 +1,63 @@
 #include "mfcc/WavReader.h"
+
+using namespace std;
+using namespace voice;
+
+WavReader::WavReader(const string& filePath) {
+  file = ifstream(filePath, ios::in | ios::binary | ios::ate);
+}
+
+WavReader::~WavReader() {
+  file.close();
+}
+
+void WavReader::init() {
+  if (file.is_open()) {
+    streampos size = file.tellg();
+    cout << "Input WAV size (bytes): " << size << endl;
+
+    file.seekg(0, ios::beg);
+    file.read((char *)&header, sizeof(WavHeader));
+
+    if (header.nChannels == 1) {
+      cout << "Mono" << endl;
+    } else {
+      cout << "Stereo" << endl;
+    }
+    cout << "Sample Rate: " << header.nSamplesPerSec << endl;
+    cout << "Byte Rate: " << header.nAvgBytesPerSec << endl;
+    cout << "Bits Per Sample: " << header.wBitsPerSample << endl;
+
+    samplingRate = header.nSamplesPerSec / 1000;
+    frameLength *= samplingRate;
+    frameSpace *= samplingRate;
+
+    bufferSize = sizeof(short) * frameLength;
+
+    cout << "Sampling rate (kHz): " << samplingRate << endl;
+    cout << "FrameLength: " << frameLength << endl;
+    cout << "FrameSpace: " << frameSpace << endl;
+  } else {
+    cout << "Initilization failed." << endl;
+  }
+}
+
+int WavReader::getSamplingLength() {
+  return samplingRate;
+}
+
+int WavReader::getFrameLength() {
+  return frameLength;
+}
+
+vector<short> WavReader::read() {
+  vector<short> frame(frameLength);
+
+  file.read((char*)&frame[0], bufferSize);
+  if (file.gcount() == bufferSize) {
+    return frame;
+  } else {
+    cout << "Frame incomplete: " << file.gcount() << " bytes. Return 0 instead." << endl;
+    return vector<short>(0);
+  }
+}
