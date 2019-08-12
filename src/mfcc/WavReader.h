d668a42c77ceb318b22dd0cd4c65732188d38550
Yinlong Su <suyinlong07@gmail.com>
2019-08-11 17:26:27 -0700

    Define WavReader class

--- src/mfcc/WavReader.h
+++ src/mfcc/WavReader.h
@@ -27,6 +27,25 @@ namespace voice {
     int wSampleLength;
   };
 
+  class WavReader {
+  public:
+    explicit WavReader(const std::string& filePath);
+    ~WavReader();
+    void init();
+    int getSamplingLength();
+    int getFrameLength();
+    std::vector<short> read();
+  private:
+    std::ifstream file;
+    WavHeader header;
+
+    int samplingRate = 0; // sampling rate in KHz
+    int frameLength = 25; // Frame length in ms
+    int frameSpace = 10; // Frame space in ms
+
+    int bufferSize = sizeof(short) * frameLength; // Frame buffer size in bytes
+  };
+
 } // namespace voice
 
 #endif // VOICE_MFCC_WAVREADER_H
