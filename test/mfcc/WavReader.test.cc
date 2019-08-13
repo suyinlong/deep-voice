9a00e832e5bf6c4de45fd5e84438044d3128f02a
Yinlong Su <suyinlong07@gmail.com>
2019-08-12 21:13:33 -0700

    Format WavReader.test

--- test/mfcc/WavReader.test.cc
+++ test/mfcc/WavReader.test.cc
@@ -1,26 +1,27 @@
-#include <iostream>
+#include "mfcc/WavReader.h"
 #include "gtest/gtest.h"
 #include "MfccTestConfig.h"
-#include "mfcc/WavReader.h"
 
-using namespace voice;
+namespace voice {
 
-TEST(WavReaderTest, Initilization) {
-  WavReader reader(VOICE_MFCC_FILEPATH);
-  reader.init();
-  ASSERT_TRUE(reader.isInitialized());
-  EXPECT_EQ(550, reader.getFrameLength());
-  EXPECT_EQ(22, reader.getSamplingLength());
-}
+  TEST(WavReaderTest, Initilization) {
+    WavReader reader(VOICE_MFCC_FILEPATH);
+    reader.init();
+    ASSERT_TRUE(reader.isInitialized());
+    EXPECT_EQ(550, reader.getFrameLength());
+    EXPECT_EQ(22, reader.getSamplingLength());
+  }
 
-TEST(WavReaderTest, InitilizationFailed) {
-  WavReader reader("");
-  reader.init();
-  ASSERT_FALSE(reader.isInitialized());
-}
+  TEST(WavReaderTest, InitilizationFailed) {
+    WavReader reader("");
+    reader.init();
+    ASSERT_FALSE(reader.isInitialized());
+  }
 
-TEST(WavReaderTest, ReadFrame) {
-  WavReader reader(VOICE_MFCC_FILEPATH);
-  reader.init();
-  EXPECT_EQ(0, reader.read().size());
-}
+  TEST(WavReaderTest, ReadFrame) {
+    WavReader reader(VOICE_MFCC_FILEPATH);
+    reader.init();
+    EXPECT_EQ(0, reader.read().size());
+  }
+
+} // namespace voice
